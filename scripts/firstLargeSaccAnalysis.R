5a131c9320f960464b446e01b71ffe654220dfe9
annaMariaA <r02al13@abdn.ac.uk>
2016-04-26 11:37:07 +0100

    serial search fixation figure

--- scripts/firstLargeSaccAnalysis.R
+++ scripts/firstLargeSaccAnalysis.R
@@ -32,7 +32,7 @@ fxdat$xAOI[fxdat$xFix >  centreHalfWidth] =  1
 fxdat = filter(fxdat, 
 	targSide=="absent", 
 	trialType=="blank", 
-	difficulty=="parallel")
+	difficulty=="serial")
 
 
 aoidat = (fxdat 
@@ -40,24 +40,24 @@ aoidat = (fxdat
 		%>% summarise(
 			meanSide=mean(xAOI), 
 			nFix=length(xAOI)))
-<<<<<<< HEAD
+#<<<<<<< HEAD
 # remove entries with fewer than 100 fixations
-aoidat = filter(aoidat, nFix>3, fixNum<6)
-=======
+aoidat = filter(aoidat, nFix>6, fixNum<11)
+#=======
 # remove entries with fewer than 10 fixations
-aoidat = filter(aoidat, nFix>3, fixNum<7)
->>>>>>> origin/master
+#aoidat = filter(aoidat, nFix>9, fixNum<11)
+#>>>>>>> origin/master
 
 
 
 
 plt2 = ggplot(aoidat, aes(y=meanSide, x=fixNum, colour=session))
-plt2 = plt2 + facet_wrap(subj~difficulty, nrow=2)
+#plt2 = plt2 + facet_wrap(subj~difficulty, nrow=2)
 plt2 = plt2 + geom_smooth(se=F)
 plt2 = plt2 + scale_y_continuous(name="prop. of fixations on either side", expand=c(0,0), limits=c(-1,1), breaks=c(-1,-.5,0,.5,1), labels=c("100% left", "75% left", "50% balanced", "75% right", "100% right"))
-plt2 = plt2 + scale_x_continuous(breaks=c(1,3,5))
+plt2 = plt2 + scale_x_continuous(breaks=c(1,3,5,7,10))
 plt2 = plt2 + theme_light()
-ggsave("LeftVrightFixationsParallel.pdf", width=8, height=6)
+ggsave("LeftVrightFixationsSerialMean.pdf", width=8, height=6)
 ggsave("LeftVrightFixationsParallel.jpg", width=8, height=6)
 # model1 = lmer(scale(xFix) ~ session*trialType*difficulty
 # 	+ (trialType+difficulty+session|subj), 
