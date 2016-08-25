26a61a2a74d9a1703e13708bd2d8ac4c98f9fd9b
Alasdair Clarke <a.clarke@abdn.ac.uk>
2016-08-25 14:35:54 +0100

    objects x hist

--- scripts/firstLargeSaccAnalysisObjectNaming.R
+++ scripts/firstLargeSaccAnalysisObjectNaming.R
@@ -12,6 +12,14 @@ fxdat$session = as.factor(fxdat$session)
 
 # define x=0 to be vertical midline
 fxdat$xFix = fxdat$xFix - 512
+
+plt = ggplot(fxdat, aes(x=xFix, colour=session)) + geom_density()
+ggsave("../plots/objects/xHist.pdf")
+
+plt = ggplot(filter(fxdat, fixNum<11), aes(x=xFix, colour=session)) + geom_density()
+plt = plt + facet_wrap(~fixNum, scales="free_y")
+ggsave("../plots/objects/xHistFacet.pdf")
+
 # code into AOIs (left/centre/right)
 centreHalfWidth = 30
 fxdat$xAOI = 0
@@ -37,7 +45,8 @@ plt = plt + geom_smooth(se=F)
 plt = plt + scale_y_continuous(name="mean x position of fixation", expand=c(0,0), limits=c(-400,400))
 plt = plt + scale_x_continuous(breaks=c(1,4,7,10))
 plt = plt + theme_light()
-plt
+ggsave("../plots/objects/meanXfixPos_agg.pdf", width=8, height=6)
+
 
 ####################################
 # plot prop. left v right fixations
@@ -89,13 +98,15 @@ sxdat = (saccDat
 			meanX=mean(xDist), 
 			nFix=length(xDist)))
 # remove entries with fewer than 10 fixations
-xdat = filter(xdat, nFix>=10, fixNum<=10)
+sxdat = filter(sxdat, nFix>=10, saccNum<=10)
+
+sxdat$session = as.factor(sxdat$session)
 
 
-
-plt3 = ggplot(aoidat, aes(y=meanSide, x=fixNum, colour=session))
+plt3 = ggplot(sxdat, aes(y=meanX, x=saccNum, colour=session))
 plt3 = plt3 + geom_point(position=position_jitter(height=0.01, width=0.1))
 plt3 = plt3 + geom_smooth(se=F)
-plt3 = plt3 + scale_y_continuous(name="prop. of fixations on either side", expand=c(0,0), limits=c(-1,1), breaks=c(-1,-.5,0,.5,1), labels=c("100% left", "75% left", "50% balanced", "75% right", "100% right"))
-plt3 = plt3 + scale_x_continuous(breaks=c(1,3,5, 7, 10))
+plt3 = plt3 + scale_y_continuous(name="saccadic amplitude", expand=c(0,0), limits=c(-400,400))
+plt3 = plt3 + scale_x_continuous(name="saccade number", breaks=c(1,4, 7, 10))
 plt3 = plt3 + theme_light()
+ggsave("../plots/objects/horiSaccAmplitude_agg.pdf", width=8, height=6)
