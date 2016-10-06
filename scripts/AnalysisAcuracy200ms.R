3a74f075e26faaf80e02145b152f0c34c08e119a
annaMariaA <r02al13@abdn.ac.uk>
2016-10-06 15:00:49 +0100

    Dprime plot

--- scripts/AnalysisAcuracy200ms.R
+++ scripts/AnalysisAcuracy200ms.R
@@ -7,7 +7,7 @@ library(gridExtra)
 library(Hmisc)
 
 cbPalette <- c("#E69F00", "#56B4E9")
-
+setwd("C:/Users/Anna/Documents/GitHub/HemianopiaTraining")
 # TODO: check subjects to replace!
 # setwd("C:/Users/r02al13/Documents/GitHub/HemianopiaTraining")
 # here is a list of the subjects we want to exlude from the analysis:
@@ -16,14 +16,15 @@ cbPalette <- c("#E69F00", "#56B4E9")
 # read in acc data:
 print("Processing Acc data")
 dat <- read.csv("../data/Acc20.txt", sep="\t")
-names(dat) = c("subj","session", "trialNum","difficulty", "targPresent", "targSide", "acc")
+dat <- read.csv("data/Acc20.txt", sep="\t")
+names(dat) = c("subj","session", "trialNum","Difficulty", "targPresent", "targSide", "acc")
 dat$targPresent = as.factor(dat$targPresent)
 levels(dat$targPresent) = c("absent", "present")
 # remove some subjects
 dat$subj = as.factor(dat$subj)
 dat$session = as.factor(dat$session)
-dat$difficulty= as.factor(dat$difficulty)
-levels(dat$difficulty) = c("hetero", "homo")
+dat$Difficulty= as.factor(dat$Difficulty)
+levels(dat$Difficulty) = c("heterogeneous", "homogeneous")
 dat$session= as.factor(dat$session)
 levels(dat$session) = c("Monday", "Friday")
 
@@ -32,7 +33,8 @@ levels(dat$session) = c("Monday", "Friday")
 dat$subj = factor(dat$subj)
 
 # save!!!
-saveRDS(dat,file="../data/AccData200ms.Rda")
+saveRDS(dat,file="data/AccData200ms.Rda")
+#saveRDS(dat,file="../data/AccData200ms.Rda")
 
 
 
@@ -40,7 +42,7 @@ saveRDS(dat,file="../data/AccData200ms.Rda")
 # dat = dat[which(dat$trial>9),]
 
 accdat  = (dat
-	%>% group_by(subj, difficulty, targPresent, session)
+	%>% group_by(subj, Difficulty, targPresent, session)
 	%>% summarise(
 		accuracy = mean(acc),
 		nTrials = length(acc)))
@@ -63,7 +65,7 @@ write.csv(accdat, "../data/accDat200ms.txt", row.names=F)
 
 
 accdat2 = (accdat
-	%>%group_by(difficulty, targPresent, session)
+	%>%group_by(Difficulty, targPresent, session)
 	%>% summarise(
 		meanAccuracy = mean(accuracy),
 		std = sd(accuracy),
@@ -72,13 +74,40 @@ accdat2 = (accdat
 		lower = meanAccuracy - 1.96*stder,
 		upper = meanAccuracy + 1.96*stder))
 
-pAcc3 = ggplot(accdat2, aes(x=session, y=meanAccuracy, colour=difficulty, ymin=lower,ymax=upper)) 
+pAcc3 = ggplot(accdat2, aes(x=session, y=meanAccuracy, colour=Difficulty, ymin=lower,ymax=upper)) 
 pAcc3 = pAcc3 + geom_point() 
 pAcc3 = pAcc3 + theme_light()
 pAcc3 = pAcc3 + scale_y_continuous(name="Accuracy(%)", limits=c(0,1)) 
-pAcc3 = pAcc3 + scale_x_discrete(name="Participant")+scale_fill_manual(name="Search Difficulty", values=cbPalette)+ facet_grid(.~targPresent)
+pAcc3 = pAcc3 + scale_x_discrete(name="Session")+scale_fill_manual(name="Search Difficulty", values=cbPalette)+ facet_grid(.~targPresent)
 pAcc3 = pAcc3 + geom_errorbar()
 pAcc3
 ggsave("../plots/accuracy200msc2.jpg",dpi=600, width=6, height=4)
+ggsave("plots/accuracy200msc2.jpg",dpi=600, width=6, height=4)
 
+dat <- read.csv("data/dprime.txt", sep="\t")
+names(dat) = c("subj","session","Difficulty", "dprime")
+# remove some subjects
+dat$subj = as.factor(dat$subj)
+dat$session = as.factor(dat$session)
+dat$Difficulty= as.factor(dat$Difficulty)
+levels(dat$Difficulty) = c("heterogeneous", "homogeneous")
+dat$session= as.factor(dat$session)
+levels(dat$session) = c("Monday", "Friday")
+DIprime=(dat
+	%>%group_by(Difficulty, session)
+	%>% summarise(
+		meanDprime = mean(dprime),
+		std = sd(dprime),
+		nPeople = length(dprime),
+		stder = std/sqrt(nPeople),
+		lower = meanDprime - 1.96*stder,
+		upper = meanDprime + 1.96*stder))
 
+dP = ggplot(DIprime, aes(x=session, y=meanDprime, colour=Difficulty, ymin=lower,ymax=upper)) 
+dP = dP + geom_point() 
+dP = dP + theme_light()
+dP = dP + scale_y_continuous(name="Mean d'", limits=c(-1,5)) 
+dP = dP + scale_x_discrete(name="Session")+scale_fill_manual(name="Search Difficulty", values=cbPalette)
+dP = dP + geom_errorbar()
+dP
+ggsave("plots/Dprime.jpg",dpi=600, width=6, height=4)
